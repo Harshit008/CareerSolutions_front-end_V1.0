f391efb7a56eb3dcc299639a2cf4d303792698b4
Harshit <srivastavaharshit008@gmail.com>
2021-03-29 00:38:22 +0530

    added view-profile component

--- src/app/JobSeeker.ts
+++ src/app/JobSeeker.ts
@@ -1,9 +1,23 @@
 export interface JobSeeker{
-    jobSeekerId: number;
-    name: string;
-    password: string;
-    username: string;
-    email: string;
-    enabled: boolean;
-    created:string;
+    jobSeekerId: number,
+    name: string,
+    password: string,
+    username: string,
+    email: string,
+    enabled: boolean,
+    created:string,
+
+    experience: number,
+    previousCompany: string,
+    careerLevel: string,
+    previousctc:number,
+    certifications: string,
+    projects: string,
+    levelOfEducation:string,
+    fieldOfStudy:string,
+    college:string,
+    hscMarks: number,
+    sscMarks: number,
+    graduationMarks: number
+
 }
